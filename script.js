a7f0d129fe2e5b85ef63ae1118383fcb368b3ec4
KrisvHeij <49883345+KrisvHeij@users.noreply.github.com>
2025-12-13 22:52:40 +0100

    Updated index.html, script.js

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
 const body = document.querySelector("body");
 const toggleSwitch = document.getElementById("toggle-switch");
+const optionsContainer = document.getElementById("options-container");
+const options = document.querySelectorAll(".option");
 
-toggleSwitch.addEventListener("click", () => {
-  body.classList.toggle("dark-mode");
+
+// options.forEach((option) => {
+//   option.addEventListener("click", (e) => {
+//     console.log(e.currentTarget.id);
+//     })
+// })
+
+// Function for toggling dark mode
+function darkMode() {
+  setTimeout(() => {
+    body.classList.toggle("dark-mode");
+  }, 200);
+}
+
+
+function chooseOption(event) {
+  const option = event.target.closest(".option");
+  if (!option) {
+    return;
+  }
+  console.log(option.id);
+}
+
+// EventListeners
+toggleSwitch.addEventListener("click", darkMode);
+
+optionsContainer.addEventListener("click", (e) => {
+  chooseOption(e);
 })
