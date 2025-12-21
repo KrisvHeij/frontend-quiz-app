8b1aa20ae2a98be0f2eb9a252379f0b1c00fb310
KrisvHeij <49883345+KrisvHeij@users.noreply.github.com>
2025-12-21 21:29:48 +0100

    Started splitting functions into smaller parts

--- script.js
+++ script.js
@@ -4,9 +4,8 @@ const welcomeText = document.getElementById("welcome-text");
 const textContainer = document.getElementById("text-container");
 const options = document.getElementById("options");
 const optionsContainer = document.querySelector(".options-container");
-const answersContainer = document.querySelector(".answers-container");
 const headerQuizSubject = document.getElementById("header-quiz-subject");
-
+const answersContainer = document.querySelector(".answers-container");
 // let questionContainer, questionSubtext, question, progressBarInner, answerOptions, answerText, submitButton, selectedOption;
 let quizData = [];
 let quiz = null;
@@ -30,93 +29,9 @@ function darkMode() {
   body.classList.toggle("dark-mode");
 }
 
-// Next question
-function nextQuestion(e) {
-  // Empty options
-  options.replaceChildren();
-
-
-}
-
-// Show first Question
-function showQuestion(e) {
-  const option = e.target.closest(".option");
-
-  if (!option) {
-    return
-  } 
-    
-  const currentIndex = Number(state.selectedOptionIndex = option.dataset.index);
-  const currentQ = Number(state.currentQuestionIndex);
-
-  // Show quiz topic in header
-  const subjectImg = document.createElement("img");
-  subjectImg.classList.add("option-icon", `option-icon-${quizData[currentIndex].title.toLowerCase()}`);
-  subjectImg.src = quizData[currentIndex].icon;
-  const subjectTitle = document.createElement("p");
-  subjectTitle.className = "text-preset-4-medium";
-  subjectTitle.textContent = quizData[currentIndex].title;
-  // Append all elements to header
-  headerQuizSubject.append(subjectImg, subjectTitle);
-
-  // Question container
-  // Empty text container
-  textContainer.replaceChildren();
-  // Create element for question container
-  const questionContainer = document.createElement("div");
-  questionContainer.className = "question-container";
-  // Subtext with question number
-  const questionCountEl = document.createElement("p");
-  questionCountEl.classList.add("subtext", "text-preset-6-italic");
-  questionCountEl.textContent = `Question ${(currentQ + 1)} of ${(quizData[currentIndex].questions.length)}`;
-  // Question
-  const questionEL = document.createElement("p");
-  questionEL.className = "text-preset-3-medium";
-  questionEL.textContent = quizData[currentIndex].questions[currentIndex].question;
-  // Progressbar
-  const progressBar = document.createElement("div");
-  progressBar.className = "progress-bar";
-  const progressBarInner= document.createElement("div");
-  progressBarInner.className = "progress-inner";
-  progressBarInner.style.width = `${(currentQ + 1) * (quizData[currentIndex].questions.length)}%`;
-
-  // Append all elements to text container
-  progressBar.append(progressBarInner);
-  questionContainer.append(questionCountEl, questionEL, progressBar);
-  textContainer.append(questionContainer);
-
-  // Answer container
-  // Show answers in options container
-  options.replaceChildren();
-  
-  // Create options with answers
-  const answerOptions = quizData[currentIndex].questions[currentIndex].options;
-  answerOptions.forEach((answer, index) => {
-    const option = document.createElement("div");
-    option.classList.add("option", "answer-option");
-    const div = document.createElement("div");
-    const letter = document.createElement("p");
-    letter.classList.add("option-icon", "option-letter", "text-preset-4-medium");
-    letter.textContent = String.fromCharCode(65 + index);
-    const p = document.createElement("p");
-    p.classList.add("answer", "text-preset-4-medium");
-    p.textContent = answer;
-    const img = document.createElement("img");
-    img.classList.add("answer-icon", "hidden");
-
-    // Append elements to answer container
-    div.append(letter, p, img);
-    option.append(div);
-    answersContainer.append(option);
-    options.append(answersContainer);
-  })
-}
-
-// Function to wait for data & start quiz
-async function renderStartScreen() {
-  quizData = await getData();
-  
-  quizData.forEach((quiz, index) => {
+// Render start screen / Show quiz options
+function renderStartScreen(data) {
+  data.forEach((quiz, index) => {
     const option = document.createElement("div");
     option.className = "option";
     option.dataset.index = index;
@@ -136,13 +51,127 @@ async function renderStartScreen() {
   console.log(quizData);
 }
 
-renderStartScreen();
+// Next question
+function nextQuestion(e) {
+  // Empty options
+  options.replaceChildren();
+
+
+}
+
+// Show first Question
+function showQuestion(e) {
+
+  // 1. Bepalen welk quiz
+  const option = e.target.closest(".option");
+
+  if (!option) {
+    return
+  } 
+    
+  const selectedOption = Number(state.selectedOptionIndex = option.dataset.index);
+  const currentQ = Number(state.currentQuestionIndex);
+
+
+  // 2. Header renderen
+  // Show quiz topic in header
+  const subjectImg = document.createElement("img");
+  subjectImg.classList.add("option-icon", `option-icon-${quizData[selectedOption].title.toLowerCase()}`);
+  subjectImg.src = quizData[selectedOption].icon;
+  const subjectTitle = document.createElement("p");
+  subjectTitle.className = "text-preset-4-medium";
+  subjectTitle.textContent = quizData[selectedOption].title;
+  // Append all elements to header
+  headerQuizSubject.append(subjectImg, subjectTitle);
+
+
+  // 3. Vraag renderen
+  // Question container
+  // Empty text container
+  textContainer.replaceChildren();
+  // Create element for question container
+  const questionContainer = document.createElement("div");
+  questionContainer.className = "question-container";
+  // Subtext with question number
+  const questionCountEl = document.createElement("p");
+  questionCountEl.classList.add("subtext", "text-preset-6-italic");
+  questionCountEl.textContent = `Question ${(currentQ + 1)} of ${(quizData[selectedOption].questions.length)}`;
+  // Question
+  const questionEL = document.createElement("p");
+  questionEL.className = "text-preset-3-medium";
+  questionEL.textContent = quizData[selectedOption].questions[currentQ].question;
+  // Progressbar
+  const progressBar = document.createElement("div");
+  progressBar.className = "progress-bar";
+  const progressBarInner= document.createElement("div");
+  progressBarInner.className = "progress-inner";
+  progressBarInner.style.width = `${((currentQ + 1) / (quizData[selectedOption].questions.length)) * 100}%`;
+
+  // Append all elements to text container
+  progressBar.append(progressBarInner);
+  questionContainer.append(questionCountEl, questionEL, progressBar);
+  textContainer.append(questionContainer);
+
+
+  // 4. Antwoorden renderen
+  // Answer container
+  // Empty options container
+  options.replaceChildren();
+  
+  const answersContainer = document.createElement("div");
+  answersContainer.className = "answers-container";
+  // Create options with answers
+  const answerOptions = quizData[selectedOption].questions[currentQ].options;
+  answerOptions.forEach((answer, index) => {
+    const option = document.createElement("div");
+    option.classList.add("option", "answer-option");
+    const div = document.createElement("div");
+    const letter = document.createElement("p");
+    letter.classList.add("option-icon", "option-letter", "text-preset-4-medium");
+    letter.textContent = String.fromCharCode(65 + index);
+    const p = document.createElement("p");
+    p.classList.add("answer", "text-preset-4-medium");
+    p.textContent = answer;
+    const img = document.createElement("img");
+    img.classList.add("answer-icon", "hidden");
+
+    // Append elements to answer container
+    div.append(letter, p, img);
+    option.append(div);
+    answersContainer.append(option);
+    options.append(answersContainer);
+  })
+
+  // Create submit button
+  const submitBtn = document.createElement("button");
+  submitBtn.id = "submit-btn";
+  submitBtn.classList.add("btn", "text-preset-4-medium");
+  submitBtn.textContent = "Submit answer";
+
+  // Create error message
+  const errorMsg = document.createElement("div");
+  errorMsg.id = "error-msg";
+  errorMsg.classList.add("error-msg", "hidden");
+  const errorImg = document.createElement("img");
+  errorMsg.src = "./assets/images/icon-error.svg";
+
+  // Append submit button & error message to answers container
+  answersContainer.append(submitBtn, errorMsg);
+  options.append(answersContainer);
+}
+
+// Function to wait for data & start quiz
+async function init() {
+  quizData = await getData();
+  
+  renderStartScreen(quizData);
+}
+
+init();
 
 // EventListeners
 toggleSwitch.addEventListener("click", darkMode);
 
-optionsContainer,addEventListener("click", (e) => {
+optionsContainer.addEventListener("click", (e) => {
   showQuestion(e);
 })
-
-// Eerst startQuiz om de elementen te creeeren en daarna een renderQuestion maken voor de overige vragen via de submit button
